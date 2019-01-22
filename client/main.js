ee13dccb18c740c7a37b150dd12ffee0780502bc
Marcus Francis <marcusmagnusson@outlook.com>
2019-01-22 10:57:24 +0100

    Mongo
    api/players.js

--- client/main.js
+++ client/main.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
+import { Players } from './../imports/api/players';
+import { Tracker } from 'meteor/tracker';
 
 Meteor.startup(() => {
-  let Hello = <h1> Hello World </h1>;
-  ReactDOM.render(Hello, document.getElementById("app"));
+  Tracker.autorun(() => {
+    let players = Players.find().fetch();
+
+    let Hello = <h1> Scoreboard </h1>;
+    ReactDOM.render(Hello, document.getElementById("app"));
+  });
 });
