3d284296d30f56b14de8c5dc20d56ccba4bd05b2
Marcus Francis <marcusmagnusson@outlook.com>
2019-01-22 15:00:57 +0100

    Players changing place

--- client/main.js
+++ client/main.js
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom";
 import { Meteor } from "meteor/meteor";
 import { Tracker } from "meteor/tracker";
 
-import {Players} from './../imports/api/players';
+import { Players, PlayerPositions } from "./../imports/api/players";
 import App from "./../imports/components/app";
 
 Meteor.startup(() => {
   Tracker.autorun(() => {
-    let players = Players.find().fetch();
+    let players = Players.find({}, { sort: { score: -1 } }).fetch();
+    let positionedPlayers = PlayerPositions(players);
 
-    ReactDOM.render(<App players={players} />, document.getElementById("app"));
+    ReactDOM.render(
+      <App players={positionedPlayers} />,
+      document.getElementById("app")
+    );
   });
 });
