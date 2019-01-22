5feab865f5d1c804519cf65e7ee9c5894c8373bf
Marcus Francis <marcusmagnusson@outlook.com>
2019-01-22 11:38:30 +0100

    Feat: Addplayer component

--- client/main.js
+++ client/main.js
@@ -5,12 +5,18 @@ import { Tracker } from "meteor/tracker";
 
 import { Players } from "./../imports/api/players";
 import Title from "./../imports/components/title";
+import AddPlayer from "./../imports/components/addplayer";
 
 Meteor.startup(() => {
   Tracker.autorun(() => {
     let players = Players.find().fetch();
 
-    let Hello = <Title />;
+    let Hello = (
+      <div>
+        <Title title="Scoreboard"/>
+        <AddPlayer />
+      </div>
+    );
     ReactDOM.render(Hello, document.getElementById("app"));
   });
 });
--- imports/components/title.js
+++ imports/components/title.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 export default class Title extends React.Component {
     render() {
+        
       return (
         <div>
-          <h1>Scoreboard</h1>
+          <h1>{this.props.title}</h1>
         </div>
       );
     }
