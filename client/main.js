b190bd03641f0f01fd480525fe9a1a7d444bd57e
Marcus Francis <marcusmagnusson@outlook.com>
2019-01-22 11:09:40 +0100

    Feat: Titlebar component

--- client/main.js
+++ client/main.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Meteor } from 'meteor/meteor';
-import { Players } from './../imports/api/players';
-import { Tracker } from 'meteor/tracker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Meteor } from "meteor/meteor";
+import { Tracker } from "meteor/tracker";
+
+import { Players } from "./../imports/api/players";
+import Title from "./../imports/components/title";
 
 Meteor.startup(() => {
   Tracker.autorun(() => {
     let players = Players.find().fetch();
 
-    let Hello = <h1> Scoreboard </h1>;
+    let Hello = <Title />;
     ReactDOM.render(Hello, document.getElementById("app"));
   });
 });
