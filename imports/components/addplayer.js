d60dcd3b4e18290a46a594828fa61fdc223c4d19
Marcus Francis <marcusmagnusson@outlook.com>
2019-01-26 14:29:19 +0100

    Styled addplayer component

--- imports/components/addplayer.js
+++ imports/components/addplayer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Players } from "./../api/players";
+import styled from "styled-components";
 
 export default class AddPlayer extends React.Component {
   handleSubmit = e => {
@@ -18,11 +19,38 @@ export default class AddPlayer extends React.Component {
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSubmit.bind(this)}>
-          <input type="text" name="playerName" placeholder="Name" />
-        </form>
-        <button> Add Player </button>
+        <Form onSubmit={this.handleSubmit.bind(this)}>
+          <FormInput type="text" name="playerName" placeholder="Name" />
+        </Form>
+        <Button> Add Player </Button>
       </div>
     );
   }
 }
+
+const Form = styled.form`
+  display: flex;
+  width: 100%;
+  padding: 0 1.3rem 0 1.3rem;
+  box-sizing: border-box;
+  height: 4rem;
+`;
+
+const FormInput = styled.input`
+  border: 1px solid #e8e8e8;
+  flex-grow: 1;
+  padding: 0 1.3rem 0 1.3rem;
+`;
+
+const Button = styled.button`
+  background: transparent;
+  border: 1px solid #e8e8e8;
+  color: #555555;
+  cursor: pointer;
+  font-weight: 600;
+  line-height: 1;
+  margin-left: 1.3rem;
+  outline: none;
+  padding: 1.3rem;
+  transition: background .2s ease, transform .2s ease;
+`;
